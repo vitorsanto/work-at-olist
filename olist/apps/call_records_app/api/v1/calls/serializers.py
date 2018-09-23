62c32686cfe9f8e6513e35a5b14ee2b331f7ea15
Joao Vitor Gomes <vitor.gomessanto@gmail.com>
2018-09-23 19:10:15 -0300

    Improving the API automated tests and the app's tolerance to inconsistency by adding new validation rules to the Call Record Serializer

--- olist/apps/call_records_app/api/v1/calls/serializers.py
+++ olist/apps/call_records_app/api/v1/calls/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.utils.translation import ugettext as _
 from apps.call_records_app import models
 
 
@@ -7,6 +7,30 @@ class CallRecordSerializer(serializers.ModelSerializer):
     """
     Call record model serializer
     """
+
     class Meta:
         model = models.CallRecord
         exclude = ('compromised', 'created_at')
+
+    def validate(self, data):
+        """
+        Validates the fields necessary to maintain the consistency between the call tuples
+        """
+
+        if data['call_type'] in ["", " ", None]:
+            """
+            Evaluates the call type according to its fields.
+            By doing this the application becomes more tolerant to inconsistency.
+            """
+            if data['source'] not in ["", " ", None]:
+                data['call_type'] = 1
+            else:
+                data['call_type'] = 2
+
+        if data['call_type'] == 1 and data['source'] in ["", " ", None]:
+            raise serializers.ValidationError(_('Call start records must have a source number.'))
+
+        if data['call_type'] == 2 and data['call_id'] in ["", " ", None]:
+            raise serializers.ValidationError(_('Call end records must have a call id.'))
+
+        return data
--- olist/apps/call_records_app/tests/test_api.py
+++ olist/apps/call_records_app/tests/test_api.py
@@ -17,8 +17,10 @@ class CreateRoomTest(APITestCase):
     def test_create_callrecord_with_null_fileds(self):
         """
         Tests a POST call to the callrecord API V1 using a json with null fields
-        Should allow the creation in all cases except for the one without the source number
-
+        Should allow the creation in all cases except for
+            - the case without the source number
+            - the case without call_type equals to 2
+            - the case with no data
         """
 
         self.obj_tstamp = utils.json_generator(timestamp=False)
@@ -29,15 +31,24 @@ class CreateRoomTest(APITestCase):
         response = self.client.post(reverse('v1_create_call'), self.obj_ctype)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        self.obj_cid = utils.json_generator(callid=False)
-        response = self.client.post(reverse('v1_create_call'), self.obj_cid)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
         self.obj_dest = utils.json_generator(dest=False)
         response = self.client.post(reverse('v1_create_call'), self.obj_dest)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.obj_cid = utils.json_generator(callid=False)
+        self.obj_cid['call_type'] = 1
+        response = self.client.post(reverse('v1_create_call'), self.obj_cid)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.obj_cid_2 = utils.json_generator(callid=False)
+        self.obj_cid_2['call_type'] = 2
+        response = self.client.post(reverse('v1_create_call'), self.obj_cid_2)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         self.obj_src = utils.json_generator(src=False)
         response = self.client.post(reverse('v1_create_call'), self.obj_src)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+        self.obj_no_data = utils.json_generator(timestamp=False, calltype=False, callid=False, src=False, dest=False)
+        response = self.client.post(reverse('v1_create_call'), self.obj_no_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
